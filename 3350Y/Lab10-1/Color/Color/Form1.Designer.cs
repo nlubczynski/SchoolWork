097fd725bdb015e7a6a2832f45ded602e07c85ab
nlubczynski <nlubczynski@gmail.com>
2014-01-27 22:11:13 -0500

    Exercise 2

--- 3350Y/Lab10-1/Color/Color/Form1.Designer.cs
+++ 3350Y/Lab10-1/Color/Color/Form1.Designer.cs
@@ -35,6 +35,22 @@ namespace Color
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.redButton = new System.Windows.Forms.ToolStripButton();
+            this.greenButton = new System.Windows.Forms.ToolStripButton();
+            this.blueButton = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -45,6 +61,7 @@ namespace Color
             this.button1.TabIndex = 0;
             this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -54,6 +71,7 @@ namespace Color
             this.button2.TabIndex = 1;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -86,6 +104,7 @@ namespace Color
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(205, 20);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -94,6 +113,124 @@ namespace Color
             this.textBox2.Size = new System.Drawing.Size(205, 20);
             this.textBox2.TabIndex = 5;
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(349, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.redToolStripMenuItem,
+            this.greenToolStripMenuItem,
+            this.blueToolStripMenuItem,
+            this.resetToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.colorToolStripMenuItem.Text = "Color";
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redButton_Click);
+            // 
+            // greenToolStripMenuItem
+            // 
+            this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
+            this.greenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.greenToolStripMenuItem.Text = "Green";
+            this.greenToolStripMenuItem.Click += new System.EventHandler(this.greenButton_Click);
+            // 
+            // blueToolStripMenuItem
+            // 
+            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+            this.blueToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.blueToolStripMenuItem.Text = "Blue";
+            this.blueToolStripMenuItem.Click += new System.EventHandler(this.blueButton_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.redButton,
+            this.greenButton,
+            this.blueButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(349, 25);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // redButton
+            // 
+            this.redButton.BackColor = System.Drawing.Color.Red;
+            this.redButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
+            this.redButton.Image = ((System.Drawing.Image)(resources.GetObject("redButton.Image")));
+            this.redButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(23, 22);
+            this.redButton.Text = "toolStripButton1";
+            this.redButton.Click += new System.EventHandler(this.redButton_Click);
+            // 
+            // greenButton
+            // 
+            this.greenButton.BackColor = System.Drawing.Color.Green;
+            this.greenButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
+            this.greenButton.Image = ((System.Drawing.Image)(resources.GetObject("greenButton.Image")));
+            this.greenButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.greenButton.Name = "greenButton";
+            this.greenButton.Size = new System.Drawing.Size(23, 22);
+            this.greenButton.Text = "toolStripButton2";
+            this.greenButton.Click += new System.EventHandler(this.greenButton_Click);
+            // 
+            // blueButton
+            // 
+            this.blueButton.BackColor = System.Drawing.Color.Blue;
+            this.blueButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
+            this.blueButton.Image = ((System.Drawing.Image)(resources.GetObject("blueButton.Image")));
+            this.blueButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.blueButton.Name = "blueButton";
+            this.blueButton.Size = new System.Drawing.Size(23, 22);
+            this.blueButton.Text = "toolStripButton3";
+            this.blueButton.Click += new System.EventHandler(this.blueButton_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 255);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(349, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(334, 17);
+            this.toolStripStatusLabel.Spring = true;
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -101,16 +238,26 @@ namespace Color
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(349, 277);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Color Application";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,6 +271,19 @@ namespace Color
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton redButton;
+        private System.Windows.Forms.ToolStripButton greenButton;
+        private System.Windows.Forms.ToolStripButton blueButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
     }
 }
 
--- 3350Y/Lab10-1/Color/Color/Form1.cs
+++ 3350Y/Lab10-1/Color/Color/Form1.cs
@@ -16,5 +16,62 @@ namespace Color
             InitializeComponent();
         }
 
+        private void redButton_Click(object sender, EventArgs e)
+        {
+            if(this.ActiveControl.GetType() == typeof(TextBox))
+            {
+                this.ActiveControl.BackColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void greenButton_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveControl.GetType() == typeof(TextBox))
+            {
+                this.ActiveControl.BackColor = System.Drawing.Color.Green;
+            }
+        }
+
+        private void blueButton_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveControl.GetType() == typeof(TextBox))
+            {
+                this.ActiveControl.BackColor = System.Drawing.Color.Blue;
+            }
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ActiveControl.BackColor = new System.Drawing.Color();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if( MessageBox.Show("Do you really want to exit?", "Confirm Exit", MessageBoxButtons.YesNo) == DialogResult.Yes )            
+                Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //do nothing
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string submittedString = "Submitted: \n";
+            submittedString += "Name: " + textBox1.Text;
+            submittedString += "\n";
+            submittedString += "Age: " + textBox2.Text;
+            MessageBox.Show(submittedString, "Submit Confirmation");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox1.BackColor = new System.Drawing.Color();
+            textBox2.BackColor = new System.Drawing.Color();
+        }
+
     }
 }
