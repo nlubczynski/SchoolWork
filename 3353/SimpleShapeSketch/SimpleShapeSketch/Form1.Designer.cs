05330ead33d84a0e5d54488661d2e2ac0b9018c7
NikLubz <nlubczynski@gmail.com>
2014-03-31 11:20:07 -0400

    Added base graphical object

--- 3353/SimpleShapeSketch/SimpleShapeSketch/Form1.Designer.cs
+++ 3353/SimpleShapeSketch/SimpleShapeSketch/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(674, 481);
+            this.ClientSize = new System.Drawing.Size(861, 481);
             this.Name = "Form1";
             this.Text = "Sketcher";
             this.ResumeLayout(false);
---
+++ 3353/SimpleShapeSketch/SimpleShapeSketch/Models/GraphicalObject.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SimpleShapeSketch
+{
+    public virtual class GraphicalObject
+    {
+        // Member Variables
+        protected Color _color;
+        protected Point _topLeft, _topRight, _bottomLeft, _bottomRight;
+        protected Graphics _graphics;
+
+        // Get / Set
+        public Color Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+        public Point TopLeft
+        {
+            get { return _topLeft; }
+        }
+        public Point TopRight
+        {
+            get { return _topRight; }
+        }
+        public Point BottomLeft
+        {
+            get { return _bottomLeft; }
+        }
+        public Point BottomRight
+        {
+            get { return _bottomRight; }
+        }
+
+        public virtual GraphicalObject(Graphics graphics, Color color, Point topLeft, Point topRight, Point bottomLeft, Point bottomRight)
+        {
+            // Graphics
+            _graphics = graphics;
+            
+            // Object Properties
+            _color = color;
+            _topLeft = topLeft;
+            _topRight = topRight;
+            _bottomLeft = bottomLeft;
+            _bottomRight = bottomRight;
+        }
+
+        // Virtual Functions
+        public virtual void paint(){ }
+        public virtual void delete() { }
+        public virtual void move(int x, int y) { }
+        public virtual void resize(int x1, int y1, int x2, int y2) { }
+
+    }
+}
