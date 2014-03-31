144de3384ad6ea1af3bd66560f7b8413b708316a
NikLubz <nlubczynski@gmail.com>
2014-03-31 16:11:38 -0400

    Undo/Redo done

--- 3353/SimpleShapeSketch/SimpleShapeSketch/Properties/Resources.Designer.cs
+++ 3353/SimpleShapeSketch/SimpleShapeSketch/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace SimpleShapeSketch.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap closedPolygon {
+            get {
+                object obj = ResourceManager.GetObject("closedPolygon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ellipse {
             get {
                 object obj = ResourceManager.GetObject("ellipse", resourceCulture);
